63812fe51246a417683f0dcabb07bd2145b8e430
Michael Skogberg <michael.skogberg@futurice.com>
2023-03-18 18:11:22 +0200

    Simplify build

--- frontend/rollup.config.ts
+++ frontend/rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import terser from "@rollup/plugin-terser"
-import url from "@rollup/plugin-url"
 import {scalajs, production, outputDir} from "./target/scalajs.rollup.config.js"
 import path from "path"
 import extractcss from "./rollup-extract-css"
@@ -43,9 +42,9 @@ const config: RollupOptions[] = [
     plugins: [
       sourcemaps({}),
       css(),
-      resolve(), // tells Rollup how to find date-fns in node_modules
-      commonjs(), // converts date-fns to ES modules
-      production && terser() // minify, but only in production
+      resolve(),
+      commonjs(),
+      production && terser()
     ],
     output: {
       dir: outputDir,
@@ -64,10 +63,6 @@ const config: RollupOptions[] = [
       styles: path.resolve(cssDir, "app.js")
     },
     plugins: [
-      url({
-        limit: 0,
-        fileName: production ? "[dirname][name].[hash][extname]" : "[dirname][name][extname]"
-      }),
       css(),
       production && terser()
     ],
